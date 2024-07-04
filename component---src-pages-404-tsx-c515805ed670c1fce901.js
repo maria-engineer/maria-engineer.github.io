f0d824f80e3b852f4d2f58919e67aaa23df5a098
maria-engineer <github@maria.engineer>
2024-07-04 20:50:47 +0100

    Updates

---
+++ component---src-pages-404-tsx-c515805ed670c1fce901.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmaria_dot_engineer=self.webpackChunkmaria_dot_engineer||[]).push([[1453],{731:function(t,e,n){n.r(e);var u=n(88),o=n(1042),r=n(7437);e.default=()=>(0,r.Y)(u.A,{currentPage:"404"},(0,r.Y)(o.A,{title:"404: Not found"}),(0,r.Y)("h1",null,"404: Not Found"),(0,r.Y)("p",null,"You just hit a route that doesn't exist... or is under construction...."))}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-c515805ed670c1fce901.js.map
