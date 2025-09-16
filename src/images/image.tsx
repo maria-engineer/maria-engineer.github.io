72d0f51d8566f948c51e3f34ebaedfdb836faa13
maria-engineer <github@maria.engineer>
2025-09-16 18:03:16 +0900

    new blog posts

--- src/images/image.tsx
+++ src/images/image.tsx
@@ -9,23 +9,24 @@ export function MusicImage() {
       width={240}
       height={200}
       src={"./chords.png"}
+      alt={"Musical chords"}
     />
   );
 }
 
 export function BarefootImage() {
-  return <StaticImage height={100} src={"./trained-coach-badge.png"} />;
+  return <StaticImage height={100} src={"./trained-coach-badge.png"} alt={"Barefoot Coaching Training Completion badge"}/>;
 }
 
 export function ICFImage() {
-  return <StaticImage height={100} src={"./icf-member-badge.png"} />;
+  return <StaticImage height={100} src={"./icf-member-badge.png"} alt={"ICF Member Badge"} />;
 }
 
 export function Gottman1Image() {
-  return <StaticImage height={50} src={"./gottman-level1.png"} />;
+  return <StaticImage height={50} src={"./gottman-level1.png"} alt={"Gottman Level 1 Certificate Badge"} />;
 }
 export function Gottman2Image() {
-  return <StaticImage height={50} src={"./gottman-level2.png"} />;
+  return <StaticImage height={50} src={"./gottman-level2.png"} alt={"Gottman Level 2 Certificate Badge"} />;
 }
 
 export function ProfileImage() {
@@ -33,6 +34,7 @@ export function ProfileImage() {
     <StaticImage
       border-radius={"500px"}
       src={"./maria-mateescu-portrait.jpeg"}
+      alt={"Photo of Maria Mateescu, Professional Coach and Software Engineer"}
     />
   );
 }
